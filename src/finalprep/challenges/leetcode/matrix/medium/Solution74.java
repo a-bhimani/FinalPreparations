592626004963511116669fa28bd61b53231eb144
Ankit Bhimani <ankitbhimani.me@gmail.com>
2019-01-22 00:04:57 -0600

    -Modified 74.

--- src/finalprep/challenges/leetcode/matrix/medium/Solution74.java
+++ src/finalprep/challenges/leetcode/matrix/medium/Solution74.java
@@ -8,8 +8,8 @@ public class Solution74{
 
   public boolean searchMatrix(int[][] matrix, int target){
     for(int ix = 0; ix < matrix.length; ix++){
-      for(int jy = 0, kz = matrix[ix].length - 1; jy <= kz; jy++, kz--){
-        if(target >= matrix[ix][jy] && target <= matrix[ix][kz]){
+      if((matrix[ix].length > 0) && target >= matrix[ix][0] && target <= matrix[ix][matrix[0].length - 1]){
+        for(int jy = 0, kz = matrix[ix].length - 1; jy <= kz; jy++, kz--){
           if(target == matrix[ix][jy] || target == matrix[ix][kz]){
             return true;
           }
