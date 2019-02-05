dd78fe4c046658fa356ad2f04c1cd25e50a9bf0e
Ankit Bhimani <ankitbhimani.me@gmail.com>
2019-02-04 22:20:50 -0600

    -Solution for 733.

---
+++ src/finalprep/challenges/leetcode/matrix/easy/Solution733.java
@@ -0,0 +1,41 @@
+package finalprep.challenges.leetcode.matrix.easy;
+
+/**
+ *
+ * @author adb
+ */
+public class Solution733{
+
+  public int[][] floodFill(int[][] image, int sr, int sc, int newColor){
+    int[][] newImg;
+
+    if(image[sr][sc] == newColor){
+      return image;
+    }
+
+    newImg = new int[image.length][image[0].length];
+    for(int ix = 0; ix < image.length; ix++){
+      for(int jy = 0; jy < image[ix].length; jy++){
+        newImg[ix][jy] = image[ix][jy];
+      }
+    }
+
+    borderFill(newImg, sr, sc, image[sr][sc], newColor);
+
+    return newImg;
+  }
+
+  private void borderFill(int[][] newImg, int i, int j, int origClr, int newClr){
+    if(i < 0 || i >= newImg.length || j < 0 || j >= newImg[0].length){
+      return;
+    }
+
+    if(newImg[i][j] == origClr){
+      newImg[i][j] = newClr;
+      borderFill(newImg, i - 1, j, origClr, newClr);
+      borderFill(newImg, i + 1, j, origClr, newClr);
+      borderFill(newImg, i, j - 1, origClr, newClr);
+      borderFill(newImg, i, j + 1, origClr, newClr);
+    }
+  }
+}
